415f7ea08151b7ff7a74b193f65b0d5a4e27558b
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2024-08-05 22:24:12 +0600

    fixx

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,12 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <div>Hero Section</div>
       {/* Trending ALL */}
       <TrendingAll />
       {/* Trending Movies */}
       <TrendingMovies />
       {/* Trending TV Shows */}
       <TrendingTvShows />
-      <div>TV Shows</div>
     </div>
   );
 };
